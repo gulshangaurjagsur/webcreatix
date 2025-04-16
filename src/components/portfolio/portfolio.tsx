34c980c206ddeb4d9bbcff4bd055a44b1adcae6c
Gulshan Gaur <127658605+gulshangaurjagsur@users.noreply.github.com>
2025-04-16 08:37:23 +0530

    Updated

--- src/components/portfolio/portfolio.tsx
+++ src/components/portfolio/portfolio.tsx
@@ -1,4 +1,4 @@
-import { Image } from "react-bootstrap";
+import Image from "next/image";
 import styles from "./portfolio.module.scss";
 import React from "react";
 const Portfolio = (props: any) => {
@@ -13,7 +13,21 @@ const Portfolio = (props: any) => {
           {compData?.item?.map((item: any, index: number) => (
             <div key={index} className="col-md-4 col-sm-6 col-xs-12">
               <div className={styles.item}>
-                <Image src={item?.imageSource} className="img-fluid"  alt="WebCreatix" />
+                <div
+                  style={{
+                    position: "relative",
+                    width: "100%",
+                    height: "300px",
+                  }}
+                >
+                  <Image
+                    src={item?.imageSource}
+                    alt="WebCreatix"
+                    fill
+                    sizes="(max-width: 768px) 100vw, 33vw"
+                    style={{ objectFit: "cover" }}
+                  />
+                </div>
                 <div className={styles.caption}>{item?.caption}</div>
               </div>
             </div>
