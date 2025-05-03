54376ffdd49b00156b67c4d6e79b9784eb7b9fd0
Gulshan Gaur <127658605+gulshangaurjagsur@users.noreply.github.com>
2025-05-03 16:44:10 +0530

    demo page added

--- src/pages/flight.tsx
+++ src/pages/flight.tsx
@@ -1,14 +1,5 @@
 import Head from "next/head";
-import aboutData from "../assets/data/aboutPage.json";
-import commonData from "../assets/data/common.json";
-import Footer from "@component/components/footer/footer";
-import Overview from "@component/components/overview/Overview";
-import Banner from "@component/components/banner/banner";
 import dynamic from "next/dynamic";
-import Benefits from "@component/components/benefits/Benefits";
-import WhyChoose from "@component/components/whyChoose/WhyChoose";
-import Message from "@component/components/message/message";
-// import Image from "next/image";
 import { Image } from "react-bootstrap";
 
 export default function WhyWebCreatix() {
@@ -18,12 +9,12 @@ export default function WhyWebCreatix() {
   return (
     <>
       <Head>
-        <title>Tripify</title>
+        <title>FlightVala</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
       <div className="bannerWrapper">
-      <Image src="../../../images/tripify.png" className="img-fluid" alt="qwdqwd" />
+      <Image src="../../../images/tripify.png" className="img-fluid" alt="FlightVala" />
     </div>
       </main>
     </>
