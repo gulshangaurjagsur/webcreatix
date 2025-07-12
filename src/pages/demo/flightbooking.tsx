888db15e58136ef3d27eb9dad7434a8568b94a12
Gulshan Gaur <127658605+gulshangaurjagsur@users.noreply.github.com>
2025-07-12 22:39:58 +0530

    demo page name change

--- src/pages/demo/flightbooking.tsx
+++ src/pages/demo/flightbooking.tsx
@@ -3,7 +3,7 @@ export default function FlightBookingPage() {
   return (
     <>
       <Head>
-        <title>Tripify</title>
+        <title>flight Booking</title>
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
