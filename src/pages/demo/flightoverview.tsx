ba7e299b3577fadb88d3eb052efa86c785a8752c
Gulshan Gaur <127658605+gulshangaurjagsur@users.noreply.github.com>
2025-07-17 16:25:09 +0530

    demo pages added

--- src/pages/demo/flightoverview.tsx
+++ src/pages/demo/flightoverview.tsx
@@ -1,16 +1,16 @@
 import Head from 'next/head';
-export default function FlightResultPage() {
+export default function FlightOverviewPage() {
   return (
     <>
       <Head>
-        <title>flight Result</title>
+        <title>flight Overview</title>
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
       </Head>
 
       <div style={{ margin: 0 }}>
-        <a href="flightoverview" style={{ border: "none" }}><img src="/images/demo1.png" alt="demo" /></a>
+        <img src="/images/overview.png" alt="demo" />
       </div>
     </>
   );
