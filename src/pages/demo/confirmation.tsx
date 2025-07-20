3d1c6ad56a1eccca310568daade074b3392c40ae
Gulshan Gaur <127658605+gulshangaurjagsur@users.noreply.github.com>
2025-07-20 15:01:13 +0530

    flight demo pages

--- src/pages/demo/confirmation.tsx
+++ src/pages/demo/confirmation.tsx
@@ -1,16 +1,16 @@
 import Head from 'next/head';
-export default function FlightPaymentPage() {
+export default function BookingConfirmationPage() {
   return (
     <>
       <Head>
-        <title>Payment</title>
+        <title>Confirmation</title>
         <meta httpEquiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
         <meta httpEquiv="Pragma" content="no-cache" />
         <meta httpEquiv="Expires" content="0" />
       </Head>
 
       <div style={{ margin: 0 }}>
-        <a href="confirmation" style={{ border: "none" }}><img src="/images/temp/4.png" alt="demo" /></a>
+        <img src="/images/temp/5.png" alt="demo" />
       </div>
     </>
   );
