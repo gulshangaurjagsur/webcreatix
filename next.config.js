40e46ffe98a7ba97b9d818cef3242ee52cd83913
Gulshan Gaur <gaur.gulshan@gmail.com>
2025-10-27 08:21:50 +0530

    updated

--- next.config.js
+++ next.config.js
@@ -1,9 +1,25 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true,
-  },
-};
-
-module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: "/sitemap.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml"
+          }
+        ]
+      },
+      {
+        source: "/sitemap-0.xml",
+        headers: [
+          {
+            key: "Content-Type",
+            value: "application/xml"
+          }
+        ]
+      }
+    ]
+  }
+}
