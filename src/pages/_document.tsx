cb369f80973b9059c1495b92606a0e3aad6abc35
Gulshan Gaur <gaur.gulshan@gmail.com>
2026-01-17 10:35:32 +0530

    google tag added

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,7 +3,25 @@ import { Html, Head, Main, NextScript } from 'next/document'
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=AW-17818948467"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'AW-17818948467');
+            `,
+          }}
+        />
+      </Head>
+
       <body>
         <Main />
         <NextScript />
