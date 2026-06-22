1a031ded578fbc867471a0e054113566ea097aba
gulshangaurjagsur <gaur.gulshan@gmail.com>
2026-06-22 09:32:15 +0530

    changes

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,31 +5,7 @@ import "../styles/common.scss";
 import "../scripts/layout.js";
 import "@component/styles/globals.css";
 import type { AppProps } from "next/app";
-import Script from "next/script";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      {/* Google Analytics 4 */}
-      <Script
-        async
-        src="https://www.googletagmanager.com/gtag/js?id=G-GG1ZT4JF8Y"
-      />
-
-      <Script id="ga4">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          window.gtag = gtag;
-
-          gtag('js', new Date());
-
-          gtag('config', 'G-GG1ZT4JF8Y');
-          gtag('config', 'AW-17818948467');
-        `}
-      </Script>
-
-      <Component {...pageProps} />
-    </>
-  );
+  return <Component {...pageProps} />;
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,10 +1,30 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        
+        {/* Google tag (GA4 + Google Ads) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-GG1ZT4JF8Y"
+        ></script>
+
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              // GA4
+              gtag('config', 'G-GG1ZT4JF8Y');
+
+              // Google Ads
+              gtag('config', 'AW-17818948467');
+            `,
+          }}
+        />
       </Head>
 
       <body>
@@ -12,5 +32,5 @@ export default function Document() {
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
